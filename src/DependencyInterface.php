d5a3b483059b6f881e9dad9c471196c07582e306
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2019-03-21 12:16:16 -0400

    Temp

--- src/DependencyInterface.php
+++ src/DependencyInterface.php
@@ -8,7 +8,37 @@ namespace NubecuLabs\Components;
  */
 interface DependencyInterface
 {
+    /**
+     * Include name and version. Example "jquery-1.11.1".
+     *
+     * @return string
+     */
     public function getId(): string;
 
+    /**
+     * Main name of the project. Example "jquery".
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * Value of the semantic version of the project. Example "1.11.1".
+     *
+     * @return string
+     */
     public function getVersion(): string;
+
+    /**
+     * Value of the semantic version of the project.
+     *
+     * @return string
+     */
+    public function getMinCompatibility(): string;
+
+    public function getMaxCompatibility(): string;
+
+    public function getMinAbsoluteCompatibility(): string;
+
+    public function getMaxAbsoluteCompatibility(): string;
 }
