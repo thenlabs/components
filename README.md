b79f87abd0a74d6e9815fd8ca690cc921ffd8a6e
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2019-06-19 12:05:28 -0400

    Temp

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 *Components* es un paquete PHP que posee una plantilla de componentes base que sirve para crear tipos de componentes personalizados. La misma cuenta con una serie de funcionalidades presentes en las arquitecturas basadas en componentes como por ejemplo la estructura de árbol, la propagación de eventos y la gestión de dependencias.
 
-*Components* básicamente es una implementación reutilizable del patrón de diseño (Composite)[https://es.wikipedia.org/wiki/Composite_(patr%C3%B3n_de_dise%C3%B1o)] por lo que es muy recomendable conocer el mismo para una mejor comprensión de este proyecto.
+*Components* básicamente es una implementación reutilizable y extensible del patrón de diseño (Composite)[https://es.wikipedia.org/wiki/Composite_(patr%C3%B3n_de_dise%C3%B1o)] por lo que es muy recomendable conocer el mismo para una mejor comprensión de este proyecto.
 
 ## Instalación.
 
--- src/ComponentInterface.php
+++ src/ComponentInterface.php
@@ -5,8 +5,14 @@ namespace NubecuLabs\Components;
 
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use NubecuLabs\Components\DependencyInterface;
 
 /**
+ * Base component contract.
+ *
+ * This type of components do not have children and can only be contained
+ * by components of type CompositeComponentInterface.
+ *
  * @author Andy Daniel Navarro Taño <andaniel05@gmail.com>
  */
 interface ComponentInterface extends DependentInterface
@@ -14,45 +20,178 @@ interface ComponentInterface extends DependentInterface
     /**
      * Returns the component identifier.
      *
-     * The component identifier is a value
+     * The component indentifier is used for reference the component internally.
+     * Should be an unique value.
      *
      * @return string
      */
     public function getId(): string;
 
+    /**
+     * Returns the component name.
+     *
+     * The component name is a value sets by the user.
+     *
+     * @return string
+     */
     public function getName(): ?string;
 
+    /**
+     * Sets the component name.
+     *
+     * @param string $name
+     */
     public function setName(?string $name): void;
 
+    /**
+     * Returns the direct parent of the component.
+     *
+     * @return CompositeComponentInterface|null
+     */
     public function getParent(): ?CompositeComponentInterface;
 
+    /**
+     * Returns an array with all the parent of the component.
+     *
+     * The first item is the direct parent while the last is the root of the tree.
+     *
+     * @return CompositeComponentInterface[]
+     */
     public function getParents(): array;
 
-    public function parents(): iterable;
-
+    /**
+     * Sets the component parent.
+     *
+     * @param CompositeComponentInterface $parent           The parent
+     * @param boolean                     $addChildToParent Indicates whether the component should be added as a child of the parent.
+     * @param boolean                     $dispatchEvents   Indicates if the operation will trigger events.
+     */
     public function setParent(?CompositeComponentInterface $parent, bool $addChildToParent = true, bool $dispatchEvents = true): void;
 
+    /**
+     * Use this method for iterate over each parent of the component.
+     *
+     * @see ComponentInterface::getParents()
+     *
+     * @return Generator
+     */
+    public function parents(): iterable;
+
+    /**
+     * Returns only the dependencies of the component.
+     *
+     * @return DependencyInterface[]
+     */
     public function getOwnDependencies(): array;
 
+    /**
+     * Returns other dependencies that the component may also have.
+     *
+     * The most common type of additional dependencies are those with other
+     * components that are referenced from the current one.
+     *
+     * @return DependencyInterface[]
+     */
     public function getAdditionalDependencies(): array;
 
+    /**
+     * Returns all the dependencies of the component.
+     *
+     * Include own and additional dependencies. If the component is an instance of
+     * CompositeComponentInterface then it also includes the dependencies of its children.
+     *
+     * @return DependencyInterface[]
+     */
+    public function getDependencies(): array;
+
+    /**
+     * Returns the event dispatcher of the component.
+     *
+     * @return EventDispatcherInterface
+     */
     public function getEventDispatcher(): EventDispatcherInterface;
 
+    /**
+     * Sets the event dispatcher.
+     *
+     * @param EventDispatcherInterface
+     */
     public function setEventDispatcher(EventDispatcherInterface $eventDispatcher): void;
 
+    /**
+     * Adds an event listener on the event dispatcher of the component.
+     *
+     * @see EventDispatcherInterface::addListener()
+     *
+     * @param  string   $eventName
+     * @param  callable $listener
+     */
     public function on(string $eventName, callable $listener): void;
 
+    /**
+     * Removes an event listener from the event dispatcher of the component.
+     *
+     * @see EventDispatcherInterface::removeListener()
+     *
+     * @param  string   $eventName
+     * @param  callable $listener
+     */
     public function off(string $eventName, callable $listener): void;
 
+    /**
+     * Dispatches an event to all registered listeners.
+     *
+     * Optionally may triggers capturing and propagation of the event on the tree.
+     *
+     * @see EventDispatcherInterface::dispatch()
+     *
+     * @param  string   $eventName
+     * @param  Event    $event     The object of the event.
+     * @param  boolean  $capture   Indicates whether to capture the event.
+     * @param  boolean  $bubbles   Indicates whether the event should be propagated.
+     */
     public function dispatchEvent(string $eventName, Event $event, bool $capture = true, bool $bubbles = true): void;
 
+    /**
+     * Returns all data assigned to the component.
+     *
+     * @return array
+     */
     public function getAllData(): array;
 
+    /**
+     * Sets a custom data to the component.
+     *
+     * @param string $key
+     * @param mixed  $value
+     */
     public function setData(string $key, $value): void;
 
+    /**
+     * Returns a data of the component.
+     *
+     * If data not exists returns null.
+     *
+     * @param  string $key
+     * @return mixed
+     */
     public function getData(string $key);
 
+    /**
+     * Checks if component has one data.
+     *
+     * @param  string  $key
+     * @return boolean
+     */
     public function hasData(string $key): bool;
 
+    /**
+     * Find a data for all the parents of the component.
+     *
+     * The search is made with a route from the direct father to the root of the tree.
+     *
+     * @param  string $key
+     * @return mixed  The first data found.
+     */
     public function getTopData(string $key);
 }
